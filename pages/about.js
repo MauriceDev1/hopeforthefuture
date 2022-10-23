8f71988ba2b0bf673bcfeef2043dbc04f7e3f797
maurice-cubicit <maurice@cubicit.co.za>
2022-10-23 11:50:42 +0200

    feat: add content for about

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,25 @@ export default function About() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero height="h-96 -mt-16"/>
-
+      <div className='flex w-full py-20 bg-gray-100'>
+        <div className='flex flex-wrap w-10/12 m-auto'>
+          <div className='flex flex-wrap md:w-2/3 md:p-10 text-gray-600'>
+            <h1 className='text-5xl md:text-6xl font-bold text-center m-auto pb-10 '>About Us</h1>
+            <p className='text-xl'>Hope for the Future was founded in 2002.
+              My so was, diagnosed with ADD at the age of 6. By the age of 8 he was diagnosed with ADHD.
+              It was completely foreign to me. I then educated myself to understand what it meant and needed to understand the support I had to give him.
+              I had to change he&apos;s, diet and realised my, son&apos;s future was in my hands.
+              Even though my son was academically sound he still had a learning disability.
+              After finding out my son was being marginalized, excluded and teachers were not being honest about he&apos;s progress and they used to tell me what I wanted to hear in stead of being honest.
+              After sitting with him and he&apos;s reading I realized my son picked up a brilliant skill to memorize he&apos;s reading.
+              After understanding he&apos;s needs I removed him without permission or a referral from he&apos;s primary school and placed him at a school fitting for he&apos;s needs.
+              My son today is a very intelligent young man and is a qualified Optic Fibre technician and DATA cable specialist.</p>
+          </div>
+          <div className='w-full mt-10 md:mt-0 md:w-1/3 rounded-xl border-4 border-white h-[50vh] md:h-auto'>
+          
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
